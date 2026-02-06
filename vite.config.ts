1f6f412cb13e18885fadb1fa6c575df17ca4f102
rojish3 <rojishranjit3@gmail.com>
2026-02-06 13:15:21 +0545

    Bugfix: Fix vite config error

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean,
   ),
-  base: "our-story",
+  base: "/our-story/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

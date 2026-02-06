5ff82aab0c50fe840bde2aa2982ffc30c4434de0
rojish3 <rojishranjit3@gmail.com>
2026-02-06 13:30:23 +0545

    Bugfix: Fix vite config error

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/our-story">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
